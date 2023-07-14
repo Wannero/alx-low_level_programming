d941e74d441fa204ff17d6dc0fd23b2765c710b8
Wannero <silver-79@live.fr>
2023-07-14 04:48:19 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ int main(void)
 	char ch1 = 'a';
 	char ch2 = 'A';
 
-	for (ch1 = 'a'; ch <= 'z'; ch++)
+	for (ch1 = 'a'; ch1 <= 'z'; ch1++)
 	{
 		putchar(ch1);
 	}
-	for (ch2 = 'A'; ch <= 'Z'; ch++)
+	for (ch2 = 'A'; ch2 <= 'Z'; ch2++)
 	{
 		putchar(ch2);
 	}
