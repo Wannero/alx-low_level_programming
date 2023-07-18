9ce17b62d64a4213d0964d99dc729532afcdd793
Wannero <silver-79@live.fr>
2023-07-18 05:01:31 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int a = 0;
+
+	while (a < 10)
+	{
+		for (i = 'a'; i <= z; i++)
+		{
+			_putchar(i);
+		}
+	putchar('\n');
+	a++;
+	}
+}
