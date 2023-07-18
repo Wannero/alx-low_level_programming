05fba8bfc86d0d5d6fac612cb8276b109729e45f
Wannero <silver-79@live.fr>
2023-07-18 08:48:56 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ * @n1: the first number
+ * @n2: the second number
+ * Return: Value of the last digit.
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
