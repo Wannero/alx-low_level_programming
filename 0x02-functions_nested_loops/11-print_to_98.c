7f07f38177cb3af199c5ba855365db88e836d294
Wannero <silver-79@live.fr>
2023-07-18 08:59:01 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,8 @@
 #include "main.h"
 /**
  * print_to_98 - prints all natural numbers from n to 98
- * @n: initial nulber
+ * followed by a new line
+ * @n: print from this number
  */
 void print_to_98(int n)
 {
