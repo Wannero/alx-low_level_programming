8d02285c137e192f67d8fdd27ada8bd2d7887442
Wannero <silver-79@live.fr>
2023-08-04 08:42:17 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: count line 
+ * @argv: contain arg
+ * 
+ * return: 0
+ */
+ 
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+		return (0);
+}
