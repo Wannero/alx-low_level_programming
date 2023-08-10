371648e702612c9fea1ae7beb5dfa2705743f61f
Wannero <silver-79@live.fr>
2023-08-10 09:16:17 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,67 +1,51 @@
 #include <stdlib.h>
-#include <stdio.h>
-
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _putchar(char c);
-
-/*functions for 101-mul.c */
 
 /**
- * error_exit - prints error with _putchar
- * and exits with 98
+ * string_nconcat - function that concatenates two strings
  *
- * Return: Error 98 and exit(98)
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes of @s2 to add to @s1 to be a new string
+ *
+ * Return: new string followed by the first @n bytes
+ *         of string 2 @s2 or NULL
  */
 
-int error_exit(void)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *err;
-	int i;
+	unsigned int l1, l2, i, j;
+	char *str;
 
-	err = "Error";
-	for (i = 0; err[i] != '\0'; i++)
-		_putchar(err[i]);
-	_putchar('\n');
-	exit(98);
-}
+	/*treat NULL as empty string*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-/**
- * check_number - checks if string has only
- * numbers
- *
- * @str: string to check
- *
- * Return: 0 is true 1 if false
- */
+	l1 = l2 = 0;
+	/*length of both s1 & s2 respectively*/
+	while (s1[l1] != '\0')
+		l1++;
+	while (s2[l2] != '\0')
+		l2++;
 
-int check_number(char *str)
-{
-	while (*str != '\0')
+	/*set n to length of s2*/
+	if (n >= l2)
+		n = l2;
+
+	str = (char *) malloc((l1 + n + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	/*add s1 to str*/
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	/*add s2 n bytes to str*/
+	for (j = 0; j < n && s2[j] != '\0'; j++)
 	{
-		if (*str < '0' || *str > '9')
-			return (1);
-		str++;
-	}
-	return (0);
-}
-
-/**
- * _length - get the length of strings
- *
- * @str: string to get length of
- *
- * Return: length of string
- */
-
-int _length(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
+		str[i] = s2[j];
 		i++;
-	return (i);
+	}
+	str[i] = '\0';
+	return (str);
 }
