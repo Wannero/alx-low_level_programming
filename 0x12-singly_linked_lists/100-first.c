29c08705b69197e9d6f8c6751312bbff092fb016
Wannero <silver-79@live.fr>
2023-08-24 08:45:08 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * premain - function that prints before main
+*/
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
