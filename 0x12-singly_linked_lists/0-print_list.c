6dd302048c50389cf8877bfa42ec36374b11bf95
Wannero <silver-79@live.fr>
2023-08-24 07:46:41 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+size_t print_list(const list_t *h)
+{
+    size_t i = 0;
+    
+    for (; h != NULL; h = h->next)
+    {
+        if (h->str != NULL)
+            printf("[%d] %s\n", h->len, h->str);
+        else
+            printf("[0] (nil)\n");
+
+        i++;
+    }
+    
+    return i;
+}
