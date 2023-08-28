71e6bf8a3a34c40bf31b4bd3036597331f820514
Wannero <silver-79@live.fr>
2023-08-28 11:25:24 -0700

    b9a gha hada yarbi idoz

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -6,7 +6,6 @@
  *
  * Return: number of elements in the freed list
  */
-
 size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
@@ -37,5 +36,6 @@ size_t free_listint_safe(listint_t **h)
 
 	*h = NULL;
 
-    return (len);
+	return (len);
+
 }
