1daefe949f7dfcd258a132e21dc8a6ffd2fa4ba0
Wannero <silver-79@live.fr>
2023-09-04 04:10:01 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 #include <elf.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * check_elf -function that Checks if a file is an ELF.
+ *
+ * @e_ident: A pointer to an array.
+ *
+ * Description: If the file is not an ELF file - exit code 98.
+ */
 
 int main(int argc, char **argv)
 {
